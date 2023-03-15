c9c0c9f0c57dd6d024d9f48e05a10ef38c17133c
NataliaVozhdaeva <LomzNat@yandex.ru>
2023-03-16 00:49:49 +0200

    feat: add validation for new comments

--- scripts.js
+++ scripts.js
@@ -1,7 +1,5 @@
-const commentsList = document.querySelector('.comments-list');
 const commentsCount = document.querySelector('.comments-count');
-const newCommentArea = document.querySelector('.comment-input');
-
+const commentsList = document.querySelector('.comments-list');
 const commentsArr = [
   { id: 0, author: 'John Dow', date: '2023-03-01 13:05', text: 'Bla-bla-bla bla-bla-bla', isLiked: true },
   { id: 1, author: 'Виталик', date: '2023-03-14 18:13', text: 'опрдукприабтмивалмивдоа влруд куакулдр ауар', isLiked: false },
@@ -17,11 +15,10 @@ commentsList.addEventListener('click', (e) => {
 
   if (e.target.classList.contains('comment-delete')) {
     e.target.closest('.comment').remove();
-    commentsCount.textContent = comments.length;
+    commentsCount.textContent = document.querySelectorAll('.comment').length;
   }
 });
 
-let count = 0;
 commentsArr.forEach((el) => {
   let commentsListItem = document.createElement('li');
   commentsListItem.classList.add('comment');
@@ -29,7 +26,7 @@ commentsArr.forEach((el) => {
 
   let commentText = document.createElement('p');
   commentText.classList.add('comment-text');
-  commentText.textContent = commentsArr[count].text;
+  commentText.textContent = el.text;
   commentsListItem.append(commentText);
 
   let commentWrapper = document.createElement('div');
@@ -37,18 +34,18 @@ commentsArr.forEach((el) => {
   commentsListItem.append(commentWrapper);
 
   let commentLikeBtn = document.createElement('button');
-  commentsArr[count].isLiked ? (commentLikeBtn.className = 'active comment-like') : (commentLikeBtn.className = 'comment-like');
+  el.isLiked ? (commentLikeBtn.className = 'active comment-like') : (commentLikeBtn.className = 'comment-like');
   commentWrapper.append(commentLikeBtn);
 
   let commentAuthor = document.createElement('span');
   commentAuthor.classList.add('comment-author');
-  commentAuthor.textContent = commentsArr[count].author;
+  commentAuthor.textContent = el.author;
   commentWrapper.append(commentAuthor);
 
   let now = new Date();
-  let commentFullDate = new Date(commentsArr[count].date);
-  let index = commentsArr[count].date.indexOf(' ');
-  let time = commentsArr[count].date.slice(index);
+  let commentFullDate = new Date(el.date);
+  let index = el.date.indexOf(' ');
+  let time = el.date.slice(index);
   let commentDate = document.createElement('span');
   commentDate.classList.add('comment-day');
 
@@ -59,13 +56,31 @@ commentsArr.forEach((el) => {
         now.getMonth() == commentFullDate.getMonth() &&
         now.getDate() - 1 == commentFullDate.getDate()
       ? 'вчера в ' + time
-      : commentsArr[count].date;
+      : el.date;
 
   commentWrapper.append(commentDate);
 
   let commentDeleteBtn = document.createElement('button');
   commentDeleteBtn.classList.add('comment-delete');
   commentWrapper.append(commentDeleteBtn);
-
-  count++;
 });
+
+//добавление нового комментария
+
+const newCommentArea = document.querySelector('.comment-input');
+const userName = document.querySelector('.name-input');
+const dateOfComment = document.querySelector('.date-input');
+
+const submitBtn = document.querySelector('.comment-submit');
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault;
+  if (!userName.value) {
+    document.querySelector('.name-input +.error').hidden = false;
+  }
+  return;
+  //commentsArr.push({ id: commentsArr.length, text: newCommentArea.value });
+});
+
+userName.oninput = function () {
+  document.querySelector('.name-input +.error').hidden = true;
+};
